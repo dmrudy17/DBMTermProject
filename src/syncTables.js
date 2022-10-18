63ea65875e4510762d76ad4115f5c867f0718c9c
KHoang99 <hkent625@gmail.com>
2022-10-18 15:06:11 -0400

    added a fetch games method
    the thing is however, this function only adds the same set of 40 games to the database
    so there would be duplicates,
    also the tags fetched returns jumbled information at this point

--- src/syncTables.js
+++ src/syncTables.js
@@ -8,116 +8,125 @@
     Genres and Platforms.
 */
 
-import axios from 'axios';
-import { supabase } from './supabase'
+import axios from "axios";
+import { supabase } from "./supabase";
 const apiKey = import.meta.env.VITE_RAWG_API_KEY;
 
+//thinking about putting this function into its own file maybe?
+//initialized fetches {40} game responses for {1} page
+export async function fetchGames() {
+  const pageSize = 40;
+  const baseURL = "https://api.rawg.io/api/games";
+  var allFetched = [];
+  for (var pageNum = 1; pageNum <= totalPagesWeWant; pageNum++) {
+    var response = await fetchPage(baseURL, pageSize, pageNum);
+
+    const fetched = response.data.results;
+    for (var i = 0; i < fetched.length; i++) {
+      allFetched.push({
+        game_id: fetched[i].id,
+        game_name: fetched[i].name,
+        //this would return all items in a response of tags including forward-slash in strings
+        //see image 1 as an example of tags
+        tags: fetched[i].tags,
+      });
+    }
+    if (!response.data.next) break;
+  }
+  await sendToDatabase(allFetched, "Games");
+}
 // clears the genres table, fetches genre pages from rawg, then sends the results to our database
 // - there are currently a total of 19 genres on rawg
 export async function syncGenres() {
+  await clearTable("clearGenresTable");
 
-    await clearTable('clearGenresTable');
+  const totalPagesWeWant = 1; // note that this will be the number of requests made
+  const pageSize = 40;
+  const baseURL = "https://api.rawg.io/api/genres";
+  var allFetched = [];
 
-    const totalPagesWeWant = 1; // note that this will be the number of requests made
-    const pageSize = 40;
-    const baseURL = 'https://api.rawg.io/api/genres';
-    var allFetched = [];
+  for (var pageNum = 1; pageNum <= totalPagesWeWant; pageNum++) {
+    var response = await fetchPage(baseURL, pageSize, pageNum);
 
-    for (var pageNum = 1; pageNum <= totalPagesWeWant; pageNum++) {
+    const fetched = response.data.results;
+    for (var i = 0; i < fetched.length; i++)
+      allFetched.push({ genre_id: fetched[i].id, name: fetched[i].name });
 
-        var response = await fetchPage(baseURL, pageSize, pageNum);
+    if (!response.data.next)
+      // if next is null, then this is the last page
+      break;
+  }
 
-        const fetched = response.data.results;
-        for (var i=0; i<fetched.length; i++) 
-            allFetched.push( { genre_id: fetched[i].id, name: fetched[i].name } );
-
-        if (!response.data.next)  // if next is null, then this is the last page
-            break;
-    }
-
-    await sendToDatabase(allFetched, 'Genres');
+  await sendToDatabase(allFetched, "Genres");
 }
 
 // clears the platforms table, fetches platform pages from rawg, then sends the results to our database
 // - there are currently a total of 51 platforms on rawg
 export async function syncPlatforms() {
+  await clearTable("clearPlatformsTable");
 
-    await clearTable('clearPlatformsTable');
+  const totalPagesWeWant = 2; // note that this will be the number of requests made
+  const pageSize = 40;
+  const baseURL = "https://api.rawg.io/api/platforms";
+  var allFetched = [];
 
-    const totalPagesWeWant = 2; // note that this will be the number of requests made
-    const pageSize = 40;
-    const baseURL = 'https://api.rawg.io/api/platforms';
-    var allFetched = [];
+  for (var pageNum = 1; pageNum <= totalPagesWeWant; pageNum++) {
+    var response = await fetchPage(baseURL, pageSize, pageNum);
 
-    for (var pageNum = 1; pageNum <= totalPagesWeWant; pageNum++) {
+    const fetched = response.data.results;
+    for (var i = 0; i < fetched.length; i++)
+      allFetched.push({ platform_id: fetched[i].id, name: fetched[i].name });
 
-        var response = await fetchPage(baseURL, pageSize, pageNum);
-        
-        const fetched = response.data.results;
-        for (var i=0; i<fetched.length; i++) 
-            allFetched.push( { platform_id: fetched[i].id, name: fetched[i].name } );
+    if (!response.data.next)
+      // if next is null, then this is the last page
+      break;
+  }
 
-        if (!response.data.next)  // if next is null, then this is the last page
-            break;
-    }
-
-    await sendToDatabase(allFetched, 'Platforms');
+  await sendToDatabase(allFetched, "Platforms");
 }
 
 // fetches a single page
 // - returns the response if .get is successful.  If not, error is displayed and return value is undefined
 async function fetchPage(baseURL, pageSize, pageNum) {
-
-    try {
-
-        const url = baseURL + `?key=${apiKey}&page_size=${pageSize}&page=${pageNum}`;
-        return await axios.get(url);
-
-    } catch (err) {
-        handleError(err);
-    }
+  try {
+    const url =
+      baseURL + `?key=${apiKey}&page_size=${pageSize}&page=${pageNum}`;
+    return await axios.get(url);
+  } catch (err) {
+    handleError(err);
+  }
 }
 
 // sends all fetched results to our database as an array of js objects
 // - if any of those in the list are already in the db, then this will fail
 async function sendToDatabase(allFetched, tableName) {
+  try {
+    const { data, error } = await supabase.from(tableName).insert(allFetched);
 
-    try {
-        
-        const { data, error } = await supabase
-                .from(tableName)
-                .insert(allFetched)
-
-        if (error) throw error
-
-    } catch (err) {
-        handleError(err);
-    }
+    if (error) throw error;
+  } catch (err) {
+    handleError(err);
+  }
 }
 
 // invoke the database function on supabase that clears to the table you want to clear
 // - includes clearGenresTable and clearPlatformsTable
 async function clearTable(db_function) {
+  try {
+    const { error } = await supabase.rpc(db_function);
 
-    try {
- 
-        const { error } = await supabase
-                .rpc(db_function)
-
-        if (error) throw error
-
-    } catch (err) {
-        handleError(err);
-    }
+    if (error) throw error;
+  } catch (err) {
+    handleError(err);
+  }
 }
 
 function handleError(e) {
-    
-    if (e.response) {
-        console.log("Server Error: ", e);
-    } else if (e.request) {
-        console.log("Network Error: ", e);
-    } else {
-        console.log("Client Error: ", e);
-    }
+  if (e.response) {
+    console.log("Server Error: ", e);
+  } else if (e.request) {
+    console.log("Network Error: ", e);
+  } else {
+    console.log("Client Error: ", e);
+  }
 }
