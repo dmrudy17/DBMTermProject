3f66407618c9ad8da826adc691ca02e5a65df3e8
dmrudy17 <dmr136@zips.uakron.edu>
2022-10-24 20:05:52 -0400

    Fix positioning of dropdown and button elements

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="w-screen h-screen bg-gradient-to-br from-black via-black to-indigo-500">
-    <UpsertAll></UpsertAll>
-    <Dropdown></Dropdown>
+    <div class="absolute m-0 top-52 left-0 right-0 space-x-4">
+      <UpsertAll></UpsertAll>
+      <Dropdown></Dropdown>
+    </div>
   </div>
 </template>
 
