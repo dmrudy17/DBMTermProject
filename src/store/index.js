a2dcd547a993c985212bc65943bf67238bc5bf30
dmrudy17 <dmr136@zips.uakron.edu>
2022-10-27 22:43:24 -0400

    Add functional GameCard component that displays and updates ratings

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,25 @@ import { reactive } from "vue";
 
 const state = reactive({
     user: null,
+    gameInfo: {
+        image: "",
+        title: "",
+        tagData: [], // array of objects { tagName: '', score: '' }
+    },
 });
 
 const methods = {
     setUser(payload) {
         state.user = payload ? payload.user : null;
+    },
+    setGameInfo(payload) {
+        if (payload) {
+            console.log("hello");
+            state.gameInfo.image = payload.image;
+            state.gameInfo.title = payload.title;
+            state.gameInfo.tagData = payload.tagData;
+            console.log(state.gameInfo.image);
+        }
     },
 };
 
---
+++ src/views/GamePage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="w-screen h-screen bg-gradient-to-br from-black via-black to-indigo-500">
+    <GameCard />
+  </div>
+</template>
+
+<script>
+import GameCard from '../components/GameCard.vue'
+
+export default {
+  components: {
+    GameCard
+  }
+}
+</script>
