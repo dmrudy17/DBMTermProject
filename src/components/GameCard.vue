3450bc2bfd57628b4e340338f647ea3bf912730e
KHoang99 <hkent625@gmail.com>
2022-10-29 00:25:50 -0400

    Added a div to show status success on key press of submit  input
    does not account for actual valid inputs
    takes up weird spanning around
    needs the inverse [submit failed try again]

--- src/components/GameCard.vue
+++ src/components/GameCard.vue
@@ -6,6 +6,23 @@
           <div class="w-full h-full flex flex-col">
             <img class="object-contain rounded-t-lg" :src="gameImage">
             <h1 class="text-white text-3xl">{{ gameTitle }}</h1>
+            <!-- these are trial and error pixels i found -->
+            <div class="absolute bottom-10 right-20 " v-if="checkForSubmit">
+              <div class="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md"
+                role="alert">
+                <div class="flex">
+                  <div class="py-1"><svg class="fill-current h-6 w-6 text-teal-500 mr-4"
+                      xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                      <path
+                        d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z" />
+                    </svg></div>
+                  <div>
+                    <p class="font-bold">Success</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
             <table class="table-auto text-white">
               <thead>
                 <tr>
@@ -20,7 +37,9 @@
                   <td v-if="tag.avg_score" class="text-center">{{ tag.avg_score }}</td>
                   <td v-else class="text-center">Not rated</td>
                   <td v-if="user">
-                    <input class="bg-gray-500 caret-white" type="number" v-model="tagRating[index]" @keyup.enter="updateTagRating(tag, tagRating[index])">
+                    <input ref="submitRating" class="bg-gray-500 caret-white" type="number" v-model="tagRating[index]"
+                      @keyup.enter="updateTagRating(tag, tagRating[index]); checkForSubmit = true"
+                      @click.stop="checkForSubmit = false">
                   </td>
                 </tr>
               </tbody>
@@ -36,28 +55,39 @@
 import store from '../store/index';
 import { computed, ref } from 'vue';
 import { updateRating_rpc } from '../rpc'
+import { useClickOutside } from '../useClickOutside'
 
 export default {
   data() {
-      return {
-        gameImage: '',
-        gameTitle: '',
-        tagData: '',
-        tagRating: [],
-      }
-    },
-    setup() {
-      const user = computed(() => store.state.user);
-      const gameImage = computed(() => store.state.gameInfo.image);
-      const gameTitle = computed(() => store.state.gameInfo.title);
-      const tagData = computed(() => store.state.gameInfo.tagData);
-      const tagRating = ref([]);
-
-      const updateTagRating = async (tag, tagRating) => {
-        await updateRating_rpc(store.state.user.id, tag.name, gameTitle.value, tagRating);
-      }
-
-      return { user, gameImage, gameTitle, tagData, updateTagRating, tagRating };
+    return {
+      gameImage: '',
+      gameTitle: '',
+      tagData: '',
+      tagRating: [],
+      checkForSubmit: false
     }
+  },
+  setup() {
+
+    //use if we want to add a way to stop the sucess popup when clicked outside
+    let checkForSubmit = false;
+    const submitRating = ref(null);
+    useClickOutside(submitRating, () => {
+      //this will not work, but it does react to click press outside of input
+      checkForSubmit = false;
+    });
+    //______________________
+
+    const user = computed(() => store.state.user);
+    const gameImage = computed(() => store.state.gameInfo.image);
+    const gameTitle = computed(() => store.state.gameInfo.title);
+    const tagData = computed(() => store.state.gameInfo.tagData);
+    const tagRating = ref([]);
+
+    const updateTagRating = async (tag, tagRating) => {
+      await updateRating_rpc(store.state.user.id, tag.name, gameTitle.value, tagRating);
+    }
+    return { submitRating, user, gameImage, gameTitle, tagData, updateTagRating, tagRating };
+  }
 }
 </script>
