70a5d190ee72e931caf920e55a5786b4c3d70c7f
dmrudy17 <dmr136@zips.uakron.edu>
2022-11-04 16:20:25 -0400

    Add Carousel components

---
+++ src/components/CarouselCard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="border rounded-lg p-4">
+    <img class="h-50 w-full object-cover rounded-md" src="../assets/pikachusad.jpg">
+    <h2 class="mt-2 text-2xl font-bold">Some Big Heading {{n}}</h2>
+    <p class="mt-2">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    </p>
+    <button class="mt-4 px-6 py-3 rounded-md bg-indigo-500">Read More</button>
+  </div>
+</template>
+
+<script>
+</script>
