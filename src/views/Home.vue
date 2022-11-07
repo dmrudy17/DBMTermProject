eb97d9bc042c2cdd03ab2df9d028caecd621b9bc
dneag <dneag@hotmail.com>
2022-11-07 14:53:53 -0500

    Add fetchAndUpsert.js

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="w-screen h-screen bg-gradient-to-br from-black via-black to-indigo-500">
     <div class="absolute m-0 top-52 left-0 right-0 space-x-4">
       <GameFilters></GameFilters>
+      <DemoButtons></DemoButtons>
     </div>
   </div>
 </template>
@@ -13,5 +14,7 @@ import GameFilters from '../components/GameFilters.vue'
 export default {
   components: {
     GameFilters,
+    DemoButtons,
   },
+}
 </script>
