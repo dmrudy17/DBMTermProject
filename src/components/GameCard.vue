a48ab3c3627ac65a80a23bef795cb809819c79bd
dmrudy17 <dmr136@zips.uakron.edu>
2022-11-27 14:25:28 -0500

    Adjust rating error message

--- src/components/GameCard.vue
+++ src/components/GameCard.vue
@@ -55,7 +55,7 @@
                 d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z" />
             </svg></div>
           <div>
-            <p class="font-bold">Invalid rating. Enter an integer from 0-10</p>
+            <p class="font-bold">Invalid rating. Enter a rating from 1-10</p>
           </div>
         </div>
       </div>
