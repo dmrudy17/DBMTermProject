c6ce513e36a1e6503a6c1d14cca67ab00813f2ad
dmrudy17 <dmr136@zips.uakron.edu>
2022-11-29 17:08:25 -0500

    Fix merging conflicts

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -3,7 +3,7 @@
     <div class="flex justify-between flex-row">
       <router-link to="/">
         <div class="flex flex-row">
-          <CubeTransparentIcon class="stroke-white h-10 w-10" />
+          <RocketLaunchIcon class="fill-white h-10 w-10" />
           <h1 class="text-white mt-1 text-2xl">DDK Review</h1>
           <button @click="callUpsertAll()" class="mx-5 px-4 py-2 bg-stone-300 rounded-lg">Get Game Data</button>
         </div>
@@ -11,16 +11,18 @@
       <div class="flex space-x-4 flex-row mr-2">
         <router-link v-if="!user" to="/login" class="text-gray-300 p-2 hover:text-white text-lg">Log In</router-link>
         <router-link v-if="!user" to="/signup" class="text-gray-300 p-2 hover:text-white text-lg">Sign Up</router-link>
-        <router-link v-if="user" to="/settings" class="text-gray-300 p-2 hover:text-white text-lg">User Settings</router-link>
-        <button v-if="user" class="text-gray-300 hover:text-white text-lg" @click="logout()">Log Out</button>
+        <router-link v-if="user" to="/settings">
+          <UserIcon class="h-8 w-8 stroke-gray-300 hover:stroke-white mt-1" />
+        </router-link>
+        <ArrowRightOnRectangleIcon v-if="user" class="stroke-gray-300 hover:stroke-white h-8 w-8 hover:cursor-pointer mt-1" @click="logout()" />
+        <!-- <button v-if="user" class="text-gray-300 hover:text-white text-lg h-10 w-8" @click="logout()">Log Out</button> -->
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { CubeTransparentIcon } from "@heroicons/vue/24/outline";
-import { upsertAll } from "../upsertTables";
+import { RocketLaunchIcon, ArrowRightOnRectangleIcon, UserIcon } from "@heroicons/vue/24/outline";
 import { supabase } from '../supabase';
 import { useRouter } from 'vue-router';
 import store from '../store/index';
@@ -39,10 +41,11 @@ export default {
     return { user, logout };
   },
   components: {
-    CubeTransparentIcon
+    RocketLaunchIcon,
+    ArrowRightOnRectangleIcon,
+    UserIcon
   },
   methods: {
-
     async callUpsertAll() {
       
       await upsertAll();
