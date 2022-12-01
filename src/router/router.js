de55d454e8ff296b85209ec7c724197e4cc4fd1a
dneag <dneag@hotmail.com>
2022-12-01 15:43:37 -0500

    Remove unused files

--- src/router/router.js
+++ src/router/router.js
@@ -5,7 +5,6 @@ import Login from "../views/Login.vue";
 
 import UserSettings from "../views/UserSettings.vue";
 import GamePage from "../views/GamePage.vue";
-import TestView from "../views/TestView.vue";
 
 const routes = [
   {
@@ -32,11 +31,6 @@ const routes = [
     path: "/game",
     name: "GamePage",
     component: GamePage,
-  },
-  {
-    path: "/test",
-    name: "TestView",
-    component: TestView,
   },
 ];
 
